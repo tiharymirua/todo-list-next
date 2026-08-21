df5719ae0b48f10acf3bfd10b1e6ce4bdc4574a1
tihary <miruashenzen@gmail.com>
2026-08-21 21:42:14 +0300

    init:instaurer base de données et squelette projet + ajout de tache opérationnel

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,69 +1,112 @@
-import Image from "next/image";
+"use client"
 
-export default function Home() {
+import { todo } from "node:test"
+import { useState, useEffect } from "react"
+
+type Todo = {
+  id: number,
+  text: string,
+  done: boolean
+}
+
+export default function Home(){
+  const [todos, setTodos] = useState<Todo[]>([])
+  const [input, setInput] = useState("")
+
+  useEffect(() => {
+    fetch("api/todos")
+      .then((res) => res.json())
+      .then((data) => setTodos(data))
+  }, [])
+
+  async function addTodo(){
+    if(input.trim() === "") return
+
+    const res = await fetch("api/todos", {
+      method: "POST",
+      headers: { "Content-Type" : "application/json"},
+      body: JSON.stringify({ text: input}),
+    })
+
+    const newTodo = await res.json()
+
+    setTodos([...todos, newTodo])
+    setInput("")
+  }
+
+  async function toggleTodo(id: number, done: boolean){
+    const res = await fetch(`/api/todos/${id}`, {
+      method: "PATCH",
+      headers: {"Content-Type" : "application/json"},
+      body: JSON.stringify({done: !done}),
+    })
+
+    const updated = await res.json()
+    setTodos(todos.map( (t) => (t.id === id ? updated : t)))
+  }
+
+  async function deleteTodo(id: number, done: boolean){
+    await fetch(`api/todos/${id}`, {
+      method: "DELETE"
+    })
+    setTodos(todos.filter((t) => t.id != id))
+  }
+
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert h-5 w-[100px]"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the{" "}
-            <code className="rounded bg-black/[.06] px-1.5 py-0.5 font-mono text-[0.9em] dark:bg-white/[.08]">
-              page.tsx
-            </code>{" "}
-            file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <main className="min-h-screen bg-gray-50 flex flex-col items-center pt-16 px-4">
+      <div className="w-full max-w-md">
+        <h1 className="text-2xl font-bold text-gray-800 mb-6 text-center">
+          Loop Habit Tracker
+        </h1>
+
+        <div className="flex gap-2 mb-6">
+          <input
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+            placeholder="Nouvelle tâche"
+            className="flex-1 text-black border border-gray-300 rounded-lg px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-400"
+            onKeyDown={(e) => {
+              if(e.key === "Enter"){
+                  addTodo()
+                }
+              } 
+            }
+          />
+          <button
+            onClick={addTodo}
+            className="bg-blue-600 cursor-pointer text-white px-4 py-2 rounded-lg hover:bg-blue-700 transition"
           >
-            <Image
-              className="dark:invert h-[14px] w-4"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={14}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
+            Ajouter
+          </button>
         </div>
-      </main>
-    </div>
+        <ul className="flex items-center bg-white border border-gray-200 rounded-lg px-4 py-2 shadow-sm space-y-2">
+          {todos.map((todo) => (
+            <li
+              key={todo.id}
+              className="flex items-center justify-around px-4 w-100"
+            >
+              <input 
+                type="checkbox" 
+                className="w-10"
+              />
+              <span
+                onClick={() => toggleTodo(todo.id, todo.done)}
+                className={`cursor-pointer ${
+                  todo.done ? "line-through text-gray-400" : "text-gray-800"
+                }`}
+              >
+                {todo.text}
+              </span>
+              <button
+                onClick={() => deleteTodo(todo.id, todo.done)}
+                className="text-red-500 hover:text-red-700 text-sm"
+              >
+                Supprimer
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </main>
   );
 }
